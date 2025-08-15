d3c0f3115c5ae3f2b74f3552e8bccc382142b657
VenkatShreenidhi <venkatshreenidhir@Venkats-MacBook-Air.local>
2025-08-15 11:20:29 +0530

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_tranformation import DataTransformation
+from src.components.data_tranformation import DataTransformationConfig
 
 # run the whole file usingh the Run from project root using -m command 
 # python -m src.components.data_ingestion
@@ -39,7 +41,11 @@ class DataIngestion:
 # when DataIngestion class is called the three paths defined in DataIngestionConfig will be saved in ingestion_config
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    
+    dataTransformation= DataTransformation()
+    dataTransformation.initiate_data_transformation(train_data,test_data)
+    
 
 
 # Explain the code
