2d2785029ceab6e46138f12ec70c1e4b07b5e864
VenkatShreenidhi <venkatshreenidhir@Venkats-MacBook-Air.local>
2025-08-15 15:50:12 +0530

    Hyper parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,8 +50,52 @@ class ModelTrainer:
                 "Catboost": CatBoostRegressor(),
                 "AdaBoost Classifier": AdaBoostRegressor()
             }
+
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                    #'splittter':['best','random'],
+                    #'max_features':['sqrt','log2']
+                },
+                "Random Forest":{
+                    #'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                   # 'splittter':['best','random'],
+                    #'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                   # 'loss':['squared_error', 'huber','absolute_error','quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                  #  'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                  #  'splittter':['best','random'],
+                  #  'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "LinearRegression":{
+
+                },
+                "K-neighbour regressor":{
+                    'n-neighbours':[5,7,9,11],
+                   # 'weights':['uniform','distance'],
+                    #'algorithms':['ball_tree','kd_tree','brute']  
+                },
+                "CatBoosting Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations': [30,50,100]
+                },
+
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    #'loss':['linear', 'square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256] 
+                },
+            }
+
+
             model_report: dict= evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,
-                                               y_test=y_test,models=models)
+                                               y_test=y_test,models=models,params=params)
             
             ## to get best model score from dict 
             best_model_score = max(sorted(model_report.values()))
@@ -76,6 +120,7 @@ class ModelTrainer:
             r2_square = r2_score(y_test,predicted)
 
             return r2_square
+        
 
             
         except Exception as e :
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from src.exception import customException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -17,26 +18,37 @@ def save_object(file_path,obj):
     except Exception as e:
         raise customException(e,sys)
     
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
-
+        
         for i in range(len(list(models))):
+            
+            model_name = list(models.keys())[i]
+            if model_name not in params:
+                params[model_name] = {}
             model = list(models.values())[i]
 
+            para = params[model_name]
+                
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+            #model.fit(X_train,y_train,)
+        # Handle verbose parameter
             try:
                 model.fit(X_train,y_train,verbose=False)
-
+        # Make predictions
             except TypeError:
                 model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
-        
+        # Calculate scores
 
             train_model_score = r2_score(y_train,y_train_pred)
             test_model_score = r2_score(y_test,y_test_pred)
-
+        # Store test score
             report[list(models.keys())[i]] = test_model_score
         return report
     except Exception as e:
